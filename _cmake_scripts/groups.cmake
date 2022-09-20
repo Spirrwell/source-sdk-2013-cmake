60a22446d39006004142ff515dd9b8ea85d8e99d
Spirrwell <Spirrwell@gmail.com>
2022-09-19 20:13:31 -0400

    [cmake] captioncompiler buildable

--- _cmake_scripts/groups.cmake
+++ _cmake_scripts/groups.cmake
@@ -8,6 +8,11 @@ if (${BUILD_GROUP} STREQUAL "game")
 	include("${SRCDIR}/tier1/tier1.cmake")
 	include("${SRCDIR}/vgui2/vgui_controls/vgui_controls.cmake")
 elseif (${BUILD_GROUP} STREQUAL "everything")
+
+	if (${IS_WINDOWS})
+		include("${SRCDIR}/utils/captioncompiler/captioncompiler.cmake")
+	endif()
+
 	include("${SRCDIR}/game/client/client_hl2mp.cmake")
 	include("${SRCDIR}/materialsystem/stdshaders/game_shader_dx9_hl2mp.cmake")
 	include("${SRCDIR}/mathlib/mathlib.cmake")
