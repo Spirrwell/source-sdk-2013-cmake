1ef5b8d5005ff49c24d26c0b5e2241e58c506d15
Spirrwell <Spirrwell@gmail.com>
2022-09-23 16:28:00 -0400

    [cmake] Add BUILD_GAME option to MP as well
    -Only hl2mp at the moment, but still

--- mp/_cmake_scripts/groups.cmake
+++ mp/_cmake_scripts/groups.cmake
@@ -1,13 +1,23 @@
 # groups.cmake
 
 if (${BUILD_GROUP} STREQUAL "game")
-	include("${SRCDIR}/game/client/client_hl2mp.cmake")
+
+	if (${BUILD_GAME} STREQUAL "hl2mp")
+		include("${SRCDIR}/game/client/client_hl2mp.cmake")
+		include("${SRCDIR}/game/server/server_hl2mp.cmake")
+	endif()
+	
 	include("${SRCDIR}/mathlib/mathlib.cmake")
 	include("${SRCDIR}/raytrace/raytrace.cmake")
-	include("${SRCDIR}/game/server/server_hl2mp.cmake")
 	include("${SRCDIR}/tier1/tier1.cmake")
 	include("${SRCDIR}/vgui2/vgui_controls/vgui_controls.cmake")
 elseif (${BUILD_GROUP} STREQUAL "everything")
+
+	if (${BUILD_GAME} STREQUAL "hl2mp")
+		include("${SRCDIR}/game/client/client_hl2mp.cmake")
+		include("${SRCDIR}/game/server/server_hl2mp.cmake")
+		include("${SRCDIR}/materialsystem/stdshaders/game_shader_dx9_hl2mp.cmake")
+	endif()
 
 	if (${IS_WINDOWS})
 		include("${SRCDIR}/utils/captioncompiler/captioncompiler.cmake")
@@ -27,17 +37,18 @@ elseif (${BUILD_GROUP} STREQUAL "everything")
 		include("${SRCDIR}/utils/vvis_launcher/vvis_launcher.cmake")
 	endif()
 
-	include("${SRCDIR}/game/client/client_hl2mp.cmake")
-	include("${SRCDIR}/materialsystem/stdshaders/game_shader_dx9_hl2mp.cmake")
 	include("${SRCDIR}/mathlib/mathlib.cmake")
 	include("${SRCDIR}/raytrace/raytrace.cmake")
-	include("${SRCDIR}/game/server/server_hl2mp.cmake")
 	include("${SRCDIR}/utils/serverplugin_sample/serverplugin_empty.cmake")
 	include("${SRCDIR}/tier1/tier1.cmake")
 	include("${SRCDIR}/vgui2/vgui_controls/vgui_controls.cmake")
 
 elseif (${BUILD_GROUP} STREQUAL "shaders")
-	include("${SRCDIR}/materialsystem/stdshaders/game_shader_dx9_hl2mp.cmake")
+
+	if (${BUILD_GAME} STREQUAL "hl2mp")
+		include("${SRCDIR}/materialsystem/stdshaders/game_shader_dx9_hl2mp.cmake")
+	endif()
+
 	include("${SRCDIR}/mathlib/mathlib.cmake")
 	include("${SRCDIR}/tier1/tier1.cmake")
 endif()
