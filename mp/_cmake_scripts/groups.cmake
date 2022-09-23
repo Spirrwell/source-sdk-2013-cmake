243e0b08991e578c80d08bc83784f9c4fe5ac9e3
Spirrwell <Spirrwell@gmail.com>
2022-09-23 16:24:49 -0400

    [cmake] Cleanup MP groups.cmake

--- mp/_cmake_scripts/groups.cmake
+++ mp/_cmake_scripts/groups.cmake
@@ -11,41 +11,13 @@ elseif (${BUILD_GROUP} STREQUAL "everything")
 
 	if (${IS_WINDOWS})
 		include("${SRCDIR}/utils/captioncompiler/captioncompiler.cmake")
-	endif()
-
-	include("${SRCDIR}/game/client/client_hl2mp.cmake")
-	
-	if (${IS_WINDOWS})
 		include("${SRCDIR}/fgdlib/fgdlib.cmake")
-	endif()
-
-	include("${SRCDIR}/materialsystem/stdshaders/game_shader_dx9_hl2mp.cmake")
-
-	if (${IS_WINDOWS})
 		include("${SRCDIR}/utils/glview/glview.cmake")
 		include("${SRCDIR}/utils/height2normal/height2normal.cmake")
-	endif()
-
-	include("${SRCDIR}/mathlib/mathlib.cmake")
-
-	if (${IS_WINDOWS})
 		include("${SRCDIR}/utils/motionmapper/motionmapper.cmake")
 		include("${SRCDIR}/utils/qc_eyes/qc_eyes.cmake")
-	endif()
-
-	include("${SRCDIR}/raytrace/raytrace.cmake")
-	include("${SRCDIR}/game/server/server_hl2mp.cmake")
-	include("${SRCDIR}/utils/serverplugin_sample/serverplugin_empty.cmake")
-	include("${SRCDIR}/tier1/tier1.cmake")
-
-	if (${IS_WINDOWS})
 		include("${SRCDIR}/utils/tgadiff/tgadiff.cmake")
 		include("${SRCDIR}/utils/vbsp/vbsp.cmake")
-	endif()
-
-	include("${SRCDIR}/vgui2/vgui_controls/vgui_controls.cmake")
-
-	if (${IS_WINDOWS})
 		include("${SRCDIR}/utils/vice/vice.cmake")
 		include("${SRCDIR}/utils/vrad/vrad_dll.cmake")
 		include("${SRCDIR}/utils/vrad_launcher/vrad_launcher.cmake")
@@ -54,6 +26,16 @@ elseif (${BUILD_GROUP} STREQUAL "everything")
 		include("${SRCDIR}/utils/vvis/vvis_dll.cmake")
 		include("${SRCDIR}/utils/vvis_launcher/vvis_launcher.cmake")
 	endif()
+
+	include("${SRCDIR}/game/client/client_hl2mp.cmake")
+	include("${SRCDIR}/materialsystem/stdshaders/game_shader_dx9_hl2mp.cmake")
+	include("${SRCDIR}/mathlib/mathlib.cmake")
+	include("${SRCDIR}/raytrace/raytrace.cmake")
+	include("${SRCDIR}/game/server/server_hl2mp.cmake")
+	include("${SRCDIR}/utils/serverplugin_sample/serverplugin_empty.cmake")
+	include("${SRCDIR}/tier1/tier1.cmake")
+	include("${SRCDIR}/vgui2/vgui_controls/vgui_controls.cmake")
+
 elseif (${BUILD_GROUP} STREQUAL "shaders")
 	include("${SRCDIR}/materialsystem/stdshaders/game_shader_dx9_hl2mp.cmake")
 	include("${SRCDIR}/mathlib/mathlib.cmake")
